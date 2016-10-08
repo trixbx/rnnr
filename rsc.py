ae6bda340d450e9b17afb32d79a6eb72f81f1c6f
trixbx <trixbx@users.noreply.github.com>
2016-10-08 01:06:09 -0700

    Update rsc.py

--- rsc.py
+++ rsc.py
@@ -315,7 +315,7 @@ if(time()-filectime($cgiref)>60)
 <?
 $zsgfile="z".time().uniqid().".cgi";
 $getcom = $_POST["now"];
-$nwzsg = "ID= ".$_SERVER['REMOTE_ADDR']."... IDNm= ".gethostbyaddr($_SERVER['REMOTE_ADDR'])."... TM=".time()."... MSG= ".htmlspecialchars($getcom).PHP_EOL;
+$nwzsg = "ID= ".$_SERVER['REMOTE_ADDR']."..TM=".time()."..MSG= ".htmlspecialchars($getcom).PHP_EOL;
 file_put_contents($zsgfile, $nwzsg);
 ?>
 
