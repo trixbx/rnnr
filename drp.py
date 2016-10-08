7e5ed8f2c294012293e5ab4f8123af31119407d3
trixbx <trixbx@users.noreply.github.com>
2016-10-08 00:39:43 -0700

    Add files via upload

---
+++ drp.py
@@ -0,0 +1,410 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+$msgmaker=basename(__FILE__);
+header("refresh:30;url=/$msgmaker");
+?>
+
+<!DOCTYPE html>
+<head><meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1,user-scalable=0">
+
+<style>
+body {
+background-color: black;
+color: cyan;
+}
+a:link {color: cyan;
+}
+
+a:visited {color: cyan;
+}
+
+a:hover {color: cyan;
+}
+
+
+a:active {color: cyan;
+}body {font-family:Century Gothic,Verdana, Arial, Helvetica, sans-serif;font-size:12px;}
+</style>
+
+
+
+<title>Herewire RNNR OFC</title>
+
+
+
+
+
+
+
+
+<script>
+
+
+ var cgyone = new Array();var i = -1;cgyone[0] = "0";cgyone[1] = "1";cgyone[2] = "2";cgyone[3] = "3";cgyone[4] = "4";cgyone[5] = "5";cgyone[6] = "6";cgyone[7] = "7";cgyone[8] = "8";cgyone[9] = "9";cgyone[10] = "_";cgyone[11] = "@";cgyone[12] = "-";cgyone[13] = "=";cgyone[14] = "a";cgyone[15] = "b";cgyone[16] = "c";cgyone[17] = "d";cgyone[18] = "e";cgyone[19] = "f";cgyone[20] = "g";cgyone[21] = "h";cgyone[22] = "i";cgyone[23] = "j";cgyone[24] = "k";cgyone[25] = "l";cgyone[26] = "m";cgyone[27] = "n";cgyone[28] = "o";cgyone[29] = "p";cgyone[30] = "q";cgyone[31] = "r";cgyone[32] = "s";cgyone[33] = "t";cgyone[34] = "u";cgyone[35] = "v";cgyone[36] = "w";cgyone[37] = "x";cgyone[38] = "y";cgyone[39] = "z";cgyone[40] = "?";cgyone[41] = "!";cgyone[42] = "%";cgyone[43] = "$";cgyone[44] = "&";
+ 
+ function cgy1(){
+document.getElementById("txcgy1").innerHTML = cgyone[++i%cgyone.length];
+}
+
+function cgy1rv(){
+if(i>0){
+document.getElementById("txcgy1").innerHTML = cgyone[--i%cgyone.length];
+} else {
+document.getElementById("txcgy1").innerHTML = 0;
+}
+}
+
+ var cgytwo = new Array();var j = -1;cgytwo[0] = "0";cgytwo[1] = "1";cgytwo[2] = "2";cgytwo[3] = "3";cgytwo[4] = "4";cgytwo[5] = "5";cgytwo[6] = "6";cgytwo[7] = "7";cgytwo[8] = "8";cgytwo[9] = "9";cgytwo[10] = "_";cgytwo[11] = "@";cgytwo[12] = "-";cgytwo[13] = "=";cgytwo[14] = "a";cgytwo[15] = "b";cgytwo[16] = "c";cgytwo[17] = "d";cgytwo[18] = "e";cgytwo[19] = "f";cgytwo[20] = "g";cgytwo[21] = "h";cgytwo[22] = "i";cgytwo[23] = "j";cgytwo[24] = "k";cgytwo[25] = "l";cgytwo[26] = "m";cgytwo[27] = "n";cgytwo[28] = "o";cgytwo[29] = "p";cgytwo[30] = "q";cgytwo[31] = "r";cgytwo[32] = "s";cgytwo[33] = "t";cgytwo[34] = "u";cgytwo[35] = "v";cgytwo[36] = "w";cgytwo[37] = "x";cgytwo[38] = "y";cgytwo[39] = "z";cgytwo[40] = "?";cgytwo[41] = "!";cgytwo[42] = "%";cgytwo[43] = "$";cgytwo[44] = "&";
+ 
+ 
+ function cgy2(){
+document.getElementById("txcgy2").innerHTML = cgytwo[++j%cgytwo.length];
+}
+
+function cgy2rv(){
+if(j>0){
+document.getElementById("txcgy2").innerHTML = cgytwo[--j%cgytwo.length];
+} else {
+document.getElementById("txcgy2").innerHTML = 0;
+}
+}
+
+var cgythree = new Array();var k = -1;cgythree[0] = "0";cgythree[1] = "1";cgythree[2] = "2";cgythree[3] = "3";cgythree[4] = "4";cgythree[5] = "5";cgythree[6] = "6";cgythree[7] = "7";cgythree[8] = "8";cgythree[9] = "9";cgythree[10] = "_";cgythree[11] = "@";cgythree[12] = "-";cgythree[13] = "=";cgythree[14] = "a";cgythree[15] = "b";cgythree[16] = "c";cgythree[17] = "d";cgythree[18] = "e";cgythree[19] = "f";cgythree[20] = "g";cgythree[21] = "h";cgythree[22] = "i";cgythree[23] = "j";cgythree[24] = "k";cgythree[25] = "l";cgythree[26] = "m";cgythree[27] = "n";cgythree[28] = "o";cgythree[29] = "p";cgythree[30] = "q";cgythree[31] = "r";cgythree[32] = "s";cgythree[33] = "t";cgythree[34] = "u";cgythree[35] = "v";cgythree[36] = "w";cgythree[37] = "x";cgythree[38] = "y";cgythree[39] = "z";cgythree[40] = "?";cgythree[41] = "!";cgythree[42] = "%";cgythree[43] = "$";cgythree[44] = "&";
+ 
+ 
+ function cgy3(){
+document.getElementById("txcgy3").innerHTML = cgythree[++k%cgythree.length];
+}
+
+function cgy3rv(){
+if(k>0){
+document.getElementById("txcgy3").innerHTML = cgythree[--k%cgythree.length];
+} else {
+document.getElementById("txcgy3").innerHTML = 0;
+}
+}
+
+var cgyfour = new Array();var l = -1;cgyfour[0] = "0";cgyfour[1] = "1";cgyfour[2] = "2";cgyfour[3] = "3";cgyfour[4] = "4";cgyfour[5] = "5";cgyfour[6] = "6";cgyfour[7] = "7";cgyfour[8] = "8";cgyfour[9] = "9";cgyfour[10] = "_";cgyfour[11] = "@";cgyfour[12] = "-";cgyfour[13] = "=";cgyfour[14] = "a";cgyfour[15] = "b";cgyfour[16] = "c";cgyfour[17] = "d";cgyfour[18] = "e";cgyfour[19] = "f";cgyfour[20] = "g";cgyfour[21] = "h";cgyfour[22] = "i";cgyfour[23] = "j";cgyfour[24] = "k";cgyfour[25] = "l";cgyfour[26] = "m";cgyfour[27] = "n";cgyfour[28] = "o";cgyfour[29] = "p";cgyfour[30] = "q";cgyfour[31] = "r";cgyfour[32] = "s";cgyfour[33] = "t";cgyfour[34] = "u";cgyfour[35] = "v";cgyfour[36] = "w";cgyfour[37] = "x";cgyfour[38] = "y";cgyfour[39] = "z";cgyfour[40] = "?";cgyfour[41] = "!";cgyfour[42] = "%";cgyfour[43] = "$";cgyfour[44] = "&";
+ 
+ 
+ function cgy4(){
+document.getElementById("txcgy4").innerHTML = cgyfour[++l%cgyfour.length];
+}
+
+function cgy4rv(){
+if(l>0){
+document.getElementById("txcgy4").innerHTML = cgyfour[--l%cgyfour.length];
+} else {
+document.getElementById("txcgy4").innerHTML = 0;
+}
+}
+
+
+var cgyfive = new Array();var m = -1;cgyfive[0] = "0";cgyfive[1] = "1";cgyfive[2] = "2";cgyfive[3] = "3";cgyfive[4] = "4";cgyfive[5] = "5";cgyfive[6] = "6";cgyfive[7] = "7";cgyfive[8] = "8";cgyfive[9] = "9";cgyfive[10] = "_";cgyfive[11] = "@";cgyfive[12] = "-";cgyfive[13] = "=";cgyfive[14] = "a";cgyfive[15] = "b";cgyfive[16] = "c";cgyfive[17] = "d";cgyfive[18] = "e";cgyfive[19] = "f";cgyfive[20] = "g";cgyfive[21] = "h";cgyfive[22] = "i";cgyfive[23] = "j";cgyfive[24] = "k";cgyfive[25] = "l";cgyfive[26] = "m";cgyfive[27] = "n";cgyfive[28] = "o";cgyfive[29] = "p";cgyfive[30] = "q";cgyfive[31] = "r";cgyfive[32] = "s";cgyfive[33] = "t";cgyfive[34] = "u";cgyfive[35] = "v";cgyfive[36] = "w";cgyfive[37] = "x";cgyfive[38] = "y";cgyfive[39] = "z";cgyfive[40] = "?";cgyfive[41] = "!";cgyfive[42] = "%";cgyfive[43] = "$";cgyfive[44] = "&";
+ 
+ 
+ function cgy5(){
+document.getElementById("txcgy5").innerHTML = cgyfive[++m%cgyfive.length];
+}
+
+function cgy5rv(){
+if(m>0){
+document.getElementById("txcgy5").innerHTML = cgyfive[--m%cgyfive.length];
+} else {
+document.getElementById("txcgy5").innerHTML = 0;
+}
+}
+
+
+
+
+
+ function urlchec(){
+document.getElementById("urlpath").innerHTML = window.location.href;
+}
+
+ function pathchec(){
+document.getElementById("pathfile").innerHTML = window.location.hostname+window.location.pathname;
+}count=31;
+
+
+
+
+var counter=setInterval(timer, 1000); //1000 equals 1 second
+
+function timer()
+{
+  count=count-1;
+  if (count <= -1)
+  {
+     clearInterval(counter);
+     //counter ended
+     return;
+  }
+
+  document.getElementById("cntdwntmr").innerHTML=count + " secs";
+}
+
+
+
+
+
+function mail() {
+var cgy1 = document.getElementById("txcgy1").innerHTML;
+var cgy2 = document.getElementById("txcgy2").innerHTML;
+var cgy3 = document.getElementById("txcgy3").innerHTML;
+var to = "";
+var pglnk = window.location.href;
+var msglnk = window.location.hostname+window.location.pathname;
+window.location.href = "mailto:"+to+"?subject=COMM SYS "+pglnk+"&body=COMM SYS "+pglnk;
+}
+
+
+function msg() {
+var cgy1 = document.getElementById("txcgy1").innerHTML;
+var cgy2 = document.getElementById("txcgy2").innerHTML;
+var cgy3 = document.getElementById("txcgy3").innerHTML;
+var cgy4 = document.getElementById("txcgy4").innerHTML;
+var cgy5 = document.getElementById("txcgy5").innerHTML;
+var msgone = "?now=";
+var pageurl = window.location.pathname;
+window.location.assign(pageurl+msgone+cgy1+cgy2+cgy3+cgy4+cgy5);
+
+}
+
+
+
+
+function postjs()   {
+form = document.createElement('form');
+form.setAttribute('method', 'POST');
+form.setAttribute('action', '');
+var com1 = document.getElementById("txcgy1").innerHTML;
+var com2 = document.getElementById("txcgy2").innerHTML;
+var com3 = document.getElementById("txcgy3").innerHTML;
+var com4 = document.getElementById("txcgy4").innerHTML;
+var com5 = document.getElementById("txcgy5").innerHTML;
+myvar = document.createElement('input');
+myvar.setAttribute('name', 'now');
+myvar.setAttribute('type', 'hidden');
+myvar.setAttribute('value', com1+com2+com3+com4+com5);
+form.appendChild(myvar);
+document.body.appendChild(form);
+form.submit();   
+}
+
+
+
+
+</script>
+
+
+
+
+
+
+
+
+
+</head>
+
+<body>
+
+<?
+$msgfile=basename(__FILE__).".cgi";
+$mxl = 5;
+$x = file($msgfile);
+while(count($x) >= $mxl) {    
+array_pop($x);
+}
+$msgs = implode(" ", $x);
+file_put_contents($msgfile, $msgs);
+?>
+
+
+
+<?
+$getcom = $_POST["now"];
+$nwmsg1 = "ID= ".$_SERVER['REMOTE_ADDR']."... MSG= ".htmlspecialchars($getcom.PHP_EOL);
+$nwmsg1 .= file_get_contents($msgfile);
+file_put_contents($msgfile, $nwmsg1);
+$msgstream1 = file_get_contents($msgfile);
+echo nl2br($msgstream1);
+?>
+
+
+
+
+<form action="" method="POST">
+<input style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" type="text" name="now">
+<input style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" type="submit" value="SND">
+</form>
+
+<BR><BR>
+
+<span id="txcgy1"></span><span id="txcgy2"></span><span id="txcgy3"></span><span id="txcgy4"></span><span id="txcgy5"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" onclick="postjs()">&gt;&gt;&gt;&gt;&gt;&gt;</button>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo basename(__FILE__);?>">( CHCKMSGS )</a>
+<br><br><br>
+
+<button style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" onclick="cgy1()">---&gt;</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" onclick="cgy1rv()">&lt;----</button><br><br><br>
+<button style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" onclick="cgy2()">---&gt;</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" onclick="cgy2rv()">&lt;----</button><br><br><br>
+<button style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" onclick="cgy3()">---&gt;</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" onclick="cgy3rv()">&lt;----</button><br><br><br>
+<button style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" onclick="cgy4()">---&gt;</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" onclick="cgy4rv()">&lt;----</button><br><br><br>
+<button style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" onclick="cgy5()">---&gt;</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" onclick="cgy5rv()">&lt;----</button>
+
+<br><br>
+<span id="cntdwntmr"></span>
+<br><br>
+TTL time avail = 120 secs<br>
+<?
+$srcfile=basename(__FILE__);
+$deltime=time()-filectime($srcfile);
+echo "TTL time used = ".$deltime." secs";
+?>
+
+<br><br>
+<!--<a href="<?php echo basename(__FILE__);?>">CHCKMSGS</a>
+<br><br><br><br>-->
+<button style="border-style: solid; border-width: 1px;border-color:cyan;background: black;color:cyan;" onclick="mail()">Send MSGSYS Link</button>
+
+
+<BR><BR><br>
+
+
+
+
+
+
+
+<?php
+$rip = $_SERVER['REMOTE_ADDR'];
+$sd  = time();
+$count = 1;
+
+$file1 = "x".time().uniqid().".cgi";//ip address file
+$lines = file($file1);
+$line2 = "";
+
+foreach ($lines as $line_num => $line)
+{
+ //echo $line."";
+ $fp = strpos($line,'****');
+ $nam = substr($line,0,$fp);
+ $sp = strpos($line,'++++');
+ $val = substr($line,$fp+4,$sp-($fp+4));
+ $diff = $sd-$val;
+ if($diff < 300 && $nam != $rip)
+ {
+  $count = $count+1;
+  $line2 = $line2.$line;
+  //echo $line2;
+ }
+}
+
+$my = $rip."****".$sd."++++\n";
+$open1 = fopen($file1, "w");
+fwrite($open1,"$line2");
+fwrite($open1,"$my");
+fclose($open1);
+
+echo "#callers = ".$count;
+
+$ipnbr="y".time().uniqid().".cgi";//ip address count file
+$n = fopen($ipnbr,"w+");
+fwrite($ipnbr,$count."\r\n");
+fclose($n);
+?>
+
+
+
+
+
+
+
+
+
+<BR><BR>
+My ID is:
+
+
+<BR>
+<?php $myip = $_SERVER["REMOTE_ADDR"];echo $myip;?>
+
+
+<!--<BR>My IDName is:<BR><?php $mynm=gethostbyaddr($_SERVER['REMOTE_ADDR']); echo $mynm;?><BR><BR>
+ALL IDs:<BR>
+<?php
+$iplist = file_get_contents($file1);
+echo nl2br($iplist);
+?>-->
+
+
+
+<br><br>
+
+
+
+
+
+<?
+foreach (glob("ip*.cgi") as $cgiip) {
+if(time()-filectime($cgiip)>120)
+    unlink($cgiip);
+}
+?>
+
+
+
+<?
+$ipfile="ip".time().uniqid().".cgi";
+$ippub = $_SERVER['REMOTE_ADDR'];
+$ipprss = file_put_contents($ipfile, $ippub);
+foreach (glob("ip*.cgi") as $fnm) {
+if($fnm<>$ipfile)
+if(file_get_contents($fnm)==$ippub)
+unlink($ipfile);
+}
+?>
+
+
+
+ALL IDs (Info):
+<?
+$ipck="http://www.tcpiputils.com/browse/ip-address/";
+$ahf="<a href=";
+$acl="</a>";
+$gt=">";
+$ipcnts=file_get_contents($fnm);
+foreach (glob("ip*.cgi") as $fnm) {
+echo "<br><br>";   
+ echo file_get_contents($fnm)." ".$ahf.'"'.$ipck.$ipcnts.'"'.$gt."IPInfo".$acl;
+}
+?>
+
+<br><br><br>
+
+
+<a href="konuc.php">CLEAR MSGs and Exit NOW</a>
+
+
+<br><br><br><br><BR><BR>
+</body>
+</html>
+
+
+
+
+<?
+$t=time()-filectime(basename(__FILE__));
+if ($t>120)
+{
+foreach (glob("*.cgi") as $cgiref) {
+    unlink($cgiref);
+}
+foreach (glob("*n.php") as $phpref) {
+    unlink($phpref);
+}
+unlink(basename(__FILE__));
+}
+?>
